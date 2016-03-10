2438a38b1f478a08232307f1d27b548eb38bc2bf
David Menéndez Hurtado <david.menendez.hurtado@scilifelab.se>
2016-03-10 20:44:29 +0100

    First commit

---
+++ dummy/test.py
@@ -0,0 +1,13 @@
+import time
+import random
+import multiprocessing
+
+def f(x):
+    time.sleep(random.random() * 10)
+    print(x)
+
+print('Ready')
+pool = multiprocessing.Pool(5)
+print('Steady')
+pool.map(f, range(1000))
+print('Done!')
---
+++ setup.py
@@ -0,0 +1,6 @@
+# -*- coding: utf8 -*-
+import setuptools
+
+setuptools.setup(name='dummy', version='0.1',
+      packages=setuptools.find_packages()
+    )
